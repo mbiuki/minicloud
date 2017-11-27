4f4dabe23075243985cd871bf89e2c1cf6d4b328
Anthony C <broac@live.ca>
2017-11-27 14:18:17 -0800

    Implement Lambda function to take picture

---
+++ WebServicesCode/LambdaFunctions/TakePicture.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk');
+const iotData = new AWS.IotData({ endpoint: process.env.AWS_IOT_ENDPOINT });
+
+function returnResult(callback, statusCode, body) {
+	callback(null, {
+		statusCode: statusCode,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+		},
+		body: JSON.stringify(body)
+	});
+}
+
+exports.handler = (event, context, callback) => {
+
+	let payload = {};
+	let date = new Date();
+	payload.timeStampEpoch = date.getTime();
+	payload.timeStampIso = date.toISOString();
+
+	const params = {
+		topic: 'takepicture',
+		payload: JSON.stringify(payload)
+	};
+
+	iotData.publish(params, (err, res) => {
+		if (err) {
+			returnResult(callback, 400, "IoT Publish Error");
+		}
+
+		console.log(res);
+		returnResult(callback, 200, payload);
+	});
+};
