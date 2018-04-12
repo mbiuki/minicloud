cbdf4945d1ef878169929565af9db9a3c47e2163
Anthony C <broac@live.ca>
2018-04-11 19:19:10 -0700

    Update Slack Slash Command for getting sensor current status and sanity checks

--- WebServices/LambdaFunctions/SlackSlashCommand.js
+++ WebServices/LambdaFunctions/SlackSlashCommand.js
@@ -3,20 +3,17 @@ var lambda = new aws.Lambda({
 	region: 'us-west-2'
 });
 var qs = require('querystring');
-
+const acceptedSensors = ["led"];
 
 function returnResult(callback, statusCode, body) {
 	callback(null, {
 		statusCode: statusCode,
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-		},
 		body: body
 	});
 }
 
 exports.handler = (event, context, callback) => {
-	var slackBody = qs.parse(event.body);
+	let slackBody = qs.parse(event.body);
 	if (slackBody.token != process.env.SLACK_TOKEN) {
 		returnResult(callback, 400, "Invalid token");
 	}
@@ -29,30 +26,74 @@ exports.handler = (event, context, callback) => {
 					returnResult(callback, 400, "Error trying to take picture");
 				}
 				if (data.Payload) {
-					returnResult(callback, 200, "Picture taken");
+					returnResult(callback, 200, JSON.stringify({ "response_type": "in_channel", text: "Picture taken" }));
 				}
 			});
 			break;
 		case "/setsensor":
-		    var params = slackBody.text.split(' ');
-		    console.log(params);
+			let params = slackBody.text.split(' ');
+			let sensorId = params[0];
+			let status = params[1];
 
-		    var sensorId = params[0];
-		    var status = params[1];
-		    console.log(sensorId);
-		    console.log(status);
-		    
-		    lambda.invoke({
+			if (acceptedSensors.indexOf(sensorId) < 0) {
+				returnResult(callback, 200, "Not an accepted sensor to modify status");
+				return;
+			}
+
+			if (status != 0 && status != 1) {
+				returnResult(callback, 200, "Invalid sensor status");
+				return;
+			}
+
+			lambda.invoke({
 				FunctionName: 'SetSensorStatus',
-				Payload: JSON.stringify({"sensorId": sensorId, "status": status})
+				Payload: JSON.stringify({ "sensorId": sensorId, "status": status })
 			}, function(error, data) {
 				if (error) {
 					returnResult(callback, 400, "Error trying to set sensor status");
 				}
 				if (data.Payload) {
-					returnResult(callback, 200, "Sensor status set");
+					returnResult(callback, 200, JSON.stringify({ "response_type": "in_channel", text: "Sensor status set" }));
 				}
 			});
 			break;
+		case "/getstatus":
+			lambda.invoke({
+				FunctionName: 'GetSensorCurrStatus',
+			}, function(error, data) {
+				if (error) {
+					returnResult(callback, 400, "Error trying to get sensor status");
+				}
+				if (data.Payload) {
+					let payload = JSON.parse(data.Payload);
+					payload = JSON.parse(payload.body)
+					let text = "";
+					for (var i = 0; i < payload.Items.length; i++) {
+						var item = payload.Items[i].payload;
+						if (item["sensorId"] == "led") {
+							text += "*LED*: ";
+							if (item["status"] == "1") {
+								text += "On\n";
+							} else {
+								text += "Off\n";
+							}
+						}
+						if (item["sensorId"] == "motion") {
+							text += "*Motion Sensor*: ";
+							if (item["status"] == "1") {
+								text += "Motion Detected\n";
+							} else {
+								text += "No Motion Detected\n";
+							}
+						}
+						if (item["sensorId"] == "temp") {
+							text += "*Temperature*: " + item["temp"] + "\xB0C\n";
+							text += "*Humidity*: " + item["humidity"] + "%\n";
+						}
+					}
+					returnResult(callback, 200, JSON.stringify({ "response_type": "in_channel", text: text }));
+				}
+			});
+
 	}
 };
